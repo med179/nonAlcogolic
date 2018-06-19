735232e24c428554d3759f62179506a8ef592efa
med179 <med179@rambler.ru>
2018-06-20 00:12:20 +0300

    New version

--- nonAlcogolic.py
+++ nonAlcogolic.py
@@ -9,14 +9,35 @@ from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.image import Image
 from kivy.uix.widget import Widget
-
+from kivy.uix.screenmanager import Screen, ScreenManager 
+#from kivy.properties import ObjectProperty
 
 Config.set('graphics', 'resizable', 1)
 Config.set('graphics', 'width', 1080)
 Config.set('graphics', 'height', 1920)
 
-class StartScreen(App):
+class Manager(App):
+
     def build(self):
+        my_screenmanager = ScreenManager()
+        screen1 = StartScreen(name='screen1')
+        screen2 = Program(name='screen2')
+        my_screenmanager.add_widget(screen1)
+        my_screenmanager.add_widget(screen2)
+
+        return my_screenmanager    
+
+
+
+
+
+class StartScreen(Screen):
+    def __init__ (self,**kwargs):
+        super (StartScreen, self).__init__(**kwargs)
+
+        my_button1 = Button(text="Go to screen 2",size_hint_y=None, size_y=100)
+        my_button1.bind(on_press=self.changer)
+
         startScreen = AnchorLayout ()
 
         #скорее всего, тут нужно использовать картинку. Будет лучше выглядеть. 
@@ -25,15 +46,22 @@ class StartScreen(App):
             on_press = self.btnPress()
             )
         startScreen.add_widget(buttonStart)
-        return startScreen
+    
+    def changer(self,*args):
+        self.manager.current = 'screen2'
         
 
     def btnPress(self):
         #тут должен быть переход на второй активити и запускаться таймеры
         pass
 
-class Program(BoxLayout):
-    def build(self):
+class Program(Screen):
+    def __init__(self,**kwargs):
+        super (Program,self).__init__(**kwargs)
+
+        my_button1 = Button(text="Go to screen 1",size_hint_y=None, size_y=100)
+        my_button1.bind(on_press=self.changer) 
+
         self.excuse = ['Я не могу больше пить']
 
         program = BoxLayout()
@@ -47,8 +75,11 @@ class Program(BoxLayout):
         program.add_widget(timerOne)
         program.add_widget(timerTwo)
         program.add_widget(buttonProposal)
+        program.add_widget(my_button1)
 
-        return program
+
+    def changer(self,*args):
+        self.manager.current = 'screen1'
 
     def btnPress(self):
     #всплывает попап с отмазкой
@@ -67,5 +98,5 @@ class Menu(BoxLayout):
 
 
 if __name__ == "__main__":
-    StartScreen().run()
+    Manager().run()
 
