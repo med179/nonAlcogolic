cc46c7817af2d4b32136d40f396507e6ca8cc0d5
naranor <naranor@gmail.com>
2018-06-21 22:10:30 +0300

    Some cleanups

--- nonAlcogolic.py
+++ nonAlcogolic.py
@@ -13,9 +13,11 @@ from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import Screen, ScreenManager 
 #from kivy.properties import ObjectProperty
 
+
 Config.set('graphics', 'resizable', 1)
 Config.set('graphics', 'width', 1080)
 Config.set('graphics', 'height', 1920)
+
 
 class NonAlcogolic(App):
 
@@ -30,85 +32,68 @@ class NonAlcogolic(App):
         return my_screenmanager    
 
 
-
-
-
 class StartScreen(Screen):
-    def __init__ (self,**kwargs):
-        super (StartScreen, self).__init__(**kwargs)
 
+    def __init__(self, **kwargs):
+        super(StartScreen, self).__init__(**kwargs)
 
     def build(self):
-        my_button1 = Button(
-            text="Go to screen 2", 
-            on_press = self.changer
-            )
-#       my_button1.bind(on_press=self.changer)
-
-        startScreen = BoxLayout ()
-
         #скорее всего, тут нужно использовать картинку. Будет лучше выглядеть.
-        buttonStart = Button(
-            text = 'Начать не пить', 
-            on_press = self.btnPress
-            )
+        startScreen = BoxLayout()
+
+        my_button1 = Button(text="Go to screen 2", on_press=self.changer)
+        # my_button1.bind(on_press=self.changer)
+        buttonStart = Button(text='Начать не пить', on_press=self.btnPress)
+
         startScreen.add_widget(buttonStart)
         startScreen.add_widget(my_button1)
+
         return startScreen
 
     def changer(self,*args):
         self.manager.current = 'screen2'
         
-
     def btnPress(self):
         #тут должен быть переход на второй активити и запускаться таймеры
-
         pass
 
+
 class Program(Screen):
-    def __init__(self,**kwargs):
-        super (Program,self).__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super(Program, self).__init__(**kwargs)
     
     def build(self):
-        my_button1 = Button(
-            text="Go to screen 1", 
-            on_press = self.changer
-            )
-#       my_button1.bind(on_press=self.changer) 
+        my_button1 = Button(text="Go to screen 1", on_press=self.changer)
+        #my_button1.bind(on_press=self.changer)
 
         self.excuse = ['Я не могу больше пить']
 
         program = BoxLayout()
         timerOne = Label()
         timerTwo = Label()
-        buttonProposal = Button(
-            text = 'Мне предложили выпить', 
-            on_press = self.btnPress
-            )
+        buttonProposal = Button(text='Мне предложили выпить', on_press=self.btnPress)
         
         program.add_widget(timerOne)
         program.add_widget(timerTwo)
         program.add_widget(buttonProposal)
         program.add_widget(my_button1)
+
         return program
 
-    def changer(self,*args):
+    def changer(self, *args):
         self.manager.current = 'screen1'
 
-    def btnPress(self):
-    #всплывает попап с отмазкой
+    def btnPress(self, *args):
+        #всплывает попап с отмазкой
         print(self.excuse[0])
         pass
-
-    
 
 
 class Menu(BoxLayout):
     def build(self):
         #тут нужно все продумать
         menu = BoxLayout()
-
-        return menu 
+        return menu
 
 
 if __name__ == "__main__":
