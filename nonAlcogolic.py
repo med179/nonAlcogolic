3be5a38ac91ac41619e2707cb88748d6ce658457
med179 <med179@rambler.ru>
2018-06-25 22:26:46 +0300

    new ver new ver

--- nonAlcogolic.py
+++ nonAlcogolic.py
@@ -11,69 +11,80 @@ from kivy.uix.popup import Popup
 from kivy.uix.image import Image
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import Screen, ScreenManager 
-from kivy.properties import ObjectProperty
+#from kivy.properties import ObjectProperty
 
 
-Config.set('graphics', 'resizable', 1)
-Config.set('graphics', 'width', 1080)
-Config.set('graphics', 'height', 1920)
+#Config.set('graphics', 'resizable', 1)
+#Config.set('graphics', 'width', 1080)
+#Config.set('graphics', 'height', 1920)
 
 
 class NonAlcogolic(App):
 
     def build(self):
         my_screenmanager = ScreenManager()
-        screen1 = StartScreen(name='screen1')
-        screen2 = Program(name='screen2')
+        screen1 = StartScreen(name='StartScreen')
+        screen2 = Program(name='ProgramScreen')
+        screen3 = Menu(name='MenuScreen')
         my_screenmanager.add_widget(screen1)
         my_screenmanager.add_widget(screen2)
-
-        my_screenmanager.current = 'screen1'
-        return my_screenmanager    
+        my_screenmanager.add_widget(screen3)
+        return my_screenmanager
 
 
 class StartScreen(Screen):
 
     def __init__(self, **kwargs):
         super(StartScreen, self).__init__(**kwargs)
-        #скорее всего, тут нужно использовать картинку. Будет лучше выглядеть.
+        
         startScreen = BoxLayout()
 
-        my_button1 = Button(text="Go to screen 2", on_press=self.changer)
-        # my_button1.bind(on_press=self.changer)
-        buttonStart = Button(text='Начать не пить', on_press=self.btnPress)
+        firstButton = Button(
+            text="Начать не пить!!!", 
+            size_hint_y=None, 
+            size_y=100
+            )
+        firstButton.bind(on_press=self.changer)
 
-        startScreen.add_widget(buttonStart)
-        startScreen.add_widget(my_button1)
+        startScreen.add_widget(firstButton)
+        self.add_widget(startScreen)
+
 
     def changer(self,*args):
-        self.manager.current = 'screen2'
-        
-    def btnPress(self):
-        #тут должен быть переход на второй активити и запускаться таймеры
-        pass
+        self.manager.current = 'ProgramScreen'
+
 
 
 class Program(Screen):
     def __init__(self, **kwargs):
         super(Program, self).__init__(**kwargs)
-        my_button1 = Button(text="Go to screen 1", on_press=self.changer)
-        #my_button1.bind(on_press=self.changer)
+        
+        program = BoxLayout()
+        menuButton = Button(
+            text="Menu", 
+            size_hint_y=None, 
+            size_y=100
+            )
+        menuButton.bind(on_press=self.changer)
 
         self.excuse = ['Я не могу больше пить']
 
-        program = BoxLayout()
-        timerOne = Label()
-        timerTwo = Label()
-        buttonProposal = Button(text='Мне предложили выпить', on_press=self.btnPress)
-        
+
+        timerOne = Label(text = 'Timer 1')
+        timerTwo = Label(text = 'Timer 2')
+        buttonProposal = Button(
+            text='Мне предложили выпить', 
+            size_hint_y=None, 
+            size_y=100
+            )
+        program.add_widget(menuButton)       
         program.add_widget(timerOne)
         program.add_widget(timerTwo)
         program.add_widget(buttonProposal)
-        program.add_widget(my_button1)
+        self.add_widget(program)
 
     def changer(self, *args):
-        self.manager.current = 'screen1'
+        self.manager.current = 'MenuScreen'
 
     def btnPress(self, *args):
         #всплывает попап с отмазкой
@@ -81,11 +92,24 @@ class Program(Screen):
         pass
 
 
-class Menu(BoxLayout):
-    def build(self):
-        #тут нужно все продумать
-        menu = BoxLayout()
-        return menu
+class Menu(Screen):
+    def __init__(self, **kwargs):
+            super(Menu, self).__init__(**kwargs)
+            
+            menuScreen = BoxLayout()
+
+            returnToProgramButton = Button(
+                text="Закрыть меню", 
+                size_hint_y=None, 
+                size_y=100
+                )
+            returnToProgramButton.bind(on_press=self.changer)
+
+            menuScreen.add_widget(returnToProgramButton)
+            self.add_widget(menuScreen)
+    
+    def changer(self,*args):
+        self.manager.current = 'ProgramScreen'
 
 
 if __name__ == "__main__":
