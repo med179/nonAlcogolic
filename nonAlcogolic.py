c92b9184f1f4c838adc3a3f15ec50d86e4e798bd
naranor <naranor@gmail.com>
2018-06-26 13:06:11 +0300

    Update nonAlcogolic.py
    Some cleanups

--- nonAlcogolic.py
+++ nonAlcogolic.py
@@ -24,116 +24,80 @@ from datetime import date
 class NonAlcogolic(App):
 
     def build(self):
-        my_screenmanager = ScreenManager()
-        screen1 = StartScreen(name='StartScreen')
-        screen2 = SecondScreen(name='SecondScreen')
-        screen3 = Program(name='ProgramScreen')
-        screen4 = Menu(name='MenuScreen')
-        my_screenmanager.add_widget(screen1)
-        my_screenmanager.add_widget(screen2)
-        my_screenmanager.add_widget(screen3)
-        my_screenmanager.add_widget(screen4)
-        return my_screenmanager
+        myScreenmanager = ScreenManager()
+        startScreen = StartScreen(name='StartScreen')
+        secondScreen = SecondScreen(name='SecondScreen')
+        programScreen = Program(name='ProgramScreen')
+        menuScreen = Menu(name='MenuScreen')
+        myScreenmanager.add_widget(startScreen)
+        myScreenmanager.add_widget(secondScreen)
+        myScreenmanager.add_widget(programScreen)
+        myScreenmanager.add_widget(menuScreen)
+        return myScreenmanager
 
+    
 class StartScreen(Screen):
 
     def __init__(self, **kwargs):
         super(StartScreen, self).__init__(**kwargs)
-        
-        startScreen = BoxLayout()
-
-        firstButton = Button(
-            text="Начать не пить!!!", 
-            size_hint_y=None, 
-            size_y=100
-            )
-        firstButton.bind(on_press=self.changer)
-
-        startScreen.add_widget(firstButton)
-        self.add_widget(startScreen)
-
+        startScreenLayout = BoxLayout()
+        firstBtn = Button(text="Начать не пить!!!", size_hint_y=None, size_y=100, on_press=self.changer)
+        startScreenLayout.add_widget(firstButton)
+        self.add_widget(startScreenLayout)
 
     def changer(self,*args):
         self.manager.current = 'SecondScreen'
 
+        
 class SecondScreen(Screen):
 
     def __init__(self, **kwargs):
-        super(SecondScreen, self).__init__(**kwargs)
-        
-        secondScreen = BoxLayout()
-
-        oneDay = Button(
-            text="Один день", 
-            size_hint_y=None, 
-            size_y=100,
-            on_press=self.changerOneDay
-            )
-        
-        oneMonth = Button(
-            text="Один месяц", 
-            size_hint_y=None, 
-            size_y=100,
-            on_press=self.changerOneMonth
-            )
-
-        oneYear = Button(
-            text="Один год", 
-            size_hint_y=None, 
-            size_y=100,
-            on_press=self.changerOneYear            
-            )
-
-        secondScreen.add_widget(oneDay)
-        secondScreen.add_widget(oneMonth)
-        secondScreen.add_widget(oneYear)
-        self.add_widget(secondScreen)
-
+        super(SecondScreen, self).__init__(**kwargs)       
+        secondScreenLayout = BoxLayout()
+        oneDayBtn   = Button(text="Один день", size_hint_y=None, size_y=100, on_press=self.changerOneDay)
+        oneMonthBtn = Button(text="Один месяц", size_hint_y=None, size_y=100, on_press=self.changerOneMonth)
+        oneYearBtn = Button(text="Один год", size_hint_y=None, size_y=100, on_press=self.changerOneYear)
+        secondScreenLayout.add_widget(oneDayBtn)
+        secondScreenLayout.add_widget(oneMonthBtn)
+        secondScreenLayout.add_widget(oneYearBtn)
+        self.add_widget(secondScreenLayout)
 
     def changerOneDay(self,*args):
         self.deltaTime = date(0, 0, 1)
         self.manager.current = 'ProgramScreen'
+        
     def changerOneMonth(self,*args):
         self.deltaTime = date(0, 1, 0)
         self.manager.current = 'ProgramScreen'
+        
     def changerOneYear(self,*args):
         self.deltaTime = date(1, 0, 0)
         self.manager.current = 'ProgramScreen'
 
-class Program(Screen):
-    def __init__(self, **kwargs):
-        super(Program, self).__init__(**kwargs)
         
-        program = BoxLayout()
-        menuButton = Button(
-            text="Menu", 
-            size_hint_y=None, 
-            size_y=100
-            )
-        menuButton.bind(on_press=self.changer)
+class Program(Screen):
 
+    def __init__(self, **kwargs):
+        super(Program, self).__init__(**kwargs)       
         self.excuse = ['Я не могу больше пить', 'Принимаю антибиотки, нельзя смешивать с алкоголем - можно сдохнуть', 'Болею, доктор запретил', 'Аллергия']
 
+        programLayout = BoxLayout()
+        timerOneLbl = Label(text = 'Timer 1')
+        timerTwoLbl = Label(text = 'Timer 2')
+        menuBtn = Button(text="Menu", size_hint_y=None, size_y=100, on_press=self.changer)
+        proposalBtn = Button(text='Мне предложили выпить', size_hint_y=None, size_y=100, on_press = self.btnPress)
+        programLayout.add_widget(menuBtn)       
+        programLayout.add_widget(timerOneLbl)
+        programLayout.add_widget(timerTwoLbl)
+        programLayout.add_widget(proposalBtn)
+        self.add_widget(programLayout)
+        
+        # эту бурду в другой метод
         presentDay = date.today()
         finalDay = presentDay + self.deltaTime
-
-
-        timerOne = Label(text = 'Timer 1')
         timerOne.text = "Сегодня" + str(presentDay)
-        timerTwo = Label(text = 'Timer 2')
         timerTwo.text = "Финал" + str(finalDay)
-        buttonProposal = Button(
-            text='Мне предложили выпить', 
-            size_hint_y=None, 
-            size_y=100,
-            on_press = self.btnPress
-            )
-        program.add_widget(menuButton)       
-        program.add_widget(timerOne)
-        program.add_widget(timerTwo)
-        program.add_widget(buttonProposal)
-        self.add_widget(program)
-
+        
     def changer(self, *args):
         self.manager.current = 'MenuScreen'
 
@@ -147,27 +111,19 @@ class Program(Screen):
                 markup = True, 
                 font_size = 20), 
                 size_hint = (.7, .5))
-
         popup.open()
         print(self.excuse[0])
         pass
 
 
 class Menu(Screen):
+    
     def __init__(self, **kwargs):
-            super(Menu, self).__init__(**kwargs)
-            
-            menuScreen = BoxLayout()
-
-            returnToProgramButton = Button(
-                text="Закрыть меню", 
-                size_hint_y=None, 
-                size_y=100
-                )
-            returnToProgramButton.bind(on_press=self.changer)
-
-            menuScreen.add_widget(returnToProgramButton)
-            self.add_widget(menuScreen)
+            super(Menu, self).__init__(**kwargs)           
+            menuScreenLayout = BoxLayout()
+            returnToProgramBtn = Button(text="Закрыть меню", size_hint_y=None, size_y=100, on_press=self.changer)
+            menuScreenLayout.add_widget(returnToProgramBtn)
+            self.add_widget(menuScreenLayout)
     
     def changer(self,*args):
         self.manager.current = 'ProgramScreen'
